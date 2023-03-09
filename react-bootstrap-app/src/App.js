dbb7bb64bc3e395bce38c3d9a2fb227dffeba4c9
Wesley Reynafarje <wesley.reynafarje@mrjaffesclass.com>
2023-03-09 12:47:32 -0800

    Started template

---
+++ react-bootstrap-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return(
+      <div className="App">
+     </div>
+  )
+}
+
+export default App;
+
+https://www.pluralsight.com/guides/how-to-set-up-a-react-bootstrap-app
