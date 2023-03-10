8ba3c6fbc26e7b079b7c868620d2dcd3888dc1e5
Wesley Reynafarje <wesley.reynafarje@mrjaffesclass.com>
2023-03-10 13:44:26 -0800

    Added Rows and Cols

--- react-bootstrap-app/src/App.js
+++ react-bootstrap-app/src/App.js
@@ -1,13 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import {Card, Col, Row} from "react-bootstrap"
 import './App.css';
+
+//https://www.pluralsight.com/guides/how-to-set-up-a-react-bootstrap-app
 
 function App() {
   return(
       <div className="App">
-     </div>
+        <Card className="Card">
+          <Card.Header className="Card-Header">
+            <Row>
+              <Col>The Biggest Bangiest Theoriest BBT episode directory</Col>
+              <Col>2 of 2</Col>
+            </Row>
+          </Card.Header>
+          <Card.Body className="Card-Body">
+            YOOOOOY
+          </Card.Body>
+        </Card> 
+      </div>
   )
 }
 
 export default App;
 
-https://www.pluralsight.com/guides/how-to-set-up-a-react-bootstrap-app
