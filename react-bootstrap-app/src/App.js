711eccd4e66c17fc16ab725283f8b4bdb153d786
Wesley Reynafarje <wesley.reynafarje@mrjaffesclass.com>
2023-03-13 12:59:14 -0700

    header done

--- react-bootstrap-app/src/App.js
+++ react-bootstrap-app/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import {Card, Col, Row} from "react-bootstrap"
 import './App.css';
 
+import bigSheldon from './images/tbbt_logo.png'
+
 //https://www.pluralsight.com/guides/how-to-set-up-a-react-bootstrap-app
 
 function App() {
@@ -11,8 +13,13 @@ function App() {
         <Card className="Card">
           <Card.Header className="Card-Header">
             <Row>
-              <Col>The Biggest Bangiest Theoriest BBT episode directory</Col>
-              <Col>2 of 2</Col>
+              <Col>
+                <Card.Title style={{fontSize:"30px", marginTop:20 }}>The Big Bang Theory</Card.Title>
+                <Card.Title style={{fontSize:"20px"}}>Episode Directory</Card.Title>
+              </Col>
+              <Col>
+                <img src={bigSheldon} alt="TBBT Logo" width="300"/>
+              </Col>
             </Row>
           </Card.Header>
           <Card.Body className="Card-Body">
